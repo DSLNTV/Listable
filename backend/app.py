4a9c18b95885ccd275366cfee8f543e8cad9aec5
Dennis <142941646+DSLNTV@users.noreply.github.com>
2025-02-24 19:20:37 +0100

    [PR]: initial frontend setup (#8)
    - initial Frontend-Setup

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from pathlib import Path
 
-app = Flask(__name__)
+
+rootDir = Path.cwd()
+
+app = Flask(__name__, template_folder=rootDir/"frontend")
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
